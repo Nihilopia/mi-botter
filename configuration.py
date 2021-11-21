8cc8e441c5c75dd57688e1dc4867c31199fda510
Emmi <28660694+Nihilopia@users.noreply.github.com>
2021-11-21 14:59:19 +0100

    exported configuration to other py file

---
+++ configuration.py
@@ -0,0 +1,6 @@
+log_filename = "bot.log"
+log_format = "[%(asctime)s.%(msecs)d] [%(levelname)s] [%(name)s] %(message)s"
+log_dateformat = "%Y%m%d %H%M%S"
+
+bot_prefix = "mib!"
+bot_owner = 233018119856062466
